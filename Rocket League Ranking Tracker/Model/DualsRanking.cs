d71ad40b4fb914f3b92c5b54075a14570f6a664c
Tony Fredriksson <tony@fredriksson.me>
2015-10-28 11:38:30 +0100

    More refactoring regarding Duel and Duals

---
+++ Rocket League Ranking Tracker/Model/DualsRanking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocket_League_Ranking_Tracker.Model
+{
+    class DualsRanking : RankingModel
+    {
+        public DualsRanking()
+        {
+            address = "\"RocketLeague.exe\"+0150354C+7c+48+80+50+18c";
+        }
+    }
+}
